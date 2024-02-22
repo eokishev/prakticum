b43d22a7e1f11ded80d261dad759704bd4717277
Evgeny Okishev <tr.nsk@yandex.com>
2024-02-22 20:05:30 +0700

    Добавлен выбор браузера из переменных окружения. Вынесен драйвер в вспомогательную страницу.

---
+++ src/main/java/page/object/BasePage.java
@@ -0,0 +1,14 @@
+package page.object;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    protected WebDriver driver;
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+    }
+}
